b515886d20384500ac9f48e6bd203f9df8d47b09
Antonio <antoniovictorbordignon@outlook.com>
2023-01-31 18:18:33 -0300

    styling-grid

--- src/components/GridMoney.tsx
+++ src/components/GridMoney.tsx
@@ -12,17 +12,21 @@ export function GridMoney(){
     let eur = .92
     
     return (
-        <div className="text-white h-96 w-96 border-solid border-2 border-sky-500">
-            <p>USD:</p>
-            <input 
-                className="bg-black" 
-                type="number" 
-                placeholder='Enter the amount'
-                value={amount}
-                onChange={(e) => setAmount(e.target.valueAsNumber)}
-            />
-            <p>EUR:</p>
-            <p id="output">{(amount * eur).toFixed(2)}</p>
+        <div className="text-green-300 h-96 w-96 flex justify-center items-center">
+            <div className="justify-center items-center p-2">
+                <p className="p-2 mb-10 text-center">USD:</p>
+                <input       
+                    className="bg-black focus:outline-none p-2 w-64 h-32 text-center" 
+                    type="number" 
+                    placeholder='Enter the amount'
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.valueAsNumber)}
+                />
+            </div>
+            <div className="justify-center items-center p-2">
+                <p className="p-2 mb-10 text-center">EUR:</p>
+                <p id="output" className="bg-black focus:outline-none p-2 w-64 h-32 flex justify-center items-center">{(amount * eur).toFixed(2)}</p>
+            </div>
         </div>
     )
 }
