ef4d84ba037f94d5c964dff68883cecb8886e892
Antonio <antoniovictorbordignon@outlook.com>
2023-02-06 16:20:44 -0300

    populating-currency-list

--- src/components/CurrencyChoice.tsx
+++ src/components/CurrencyChoice.tsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import {ChangeEvent, useState, useEffect} from 'react'
+import {ChangeEvent} from 'react'
 
 interface Props {
     onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
@@ -9,7 +8,7 @@ interface Props {
 export const CurrencyChoice: React.FC<Props> = ({ onChange, value }) => { {
 
     let currencies = [ 
-        {currency: "American Dollar", value: "USD" },
+        {currency: "United States Dollar", value: "USD" },
         {currency: "Euro", value: "EUR" },
         {currency: "Real", value: "BRL" },
         {currency: "Canadian Dollar", value: "CAD" },
@@ -17,11 +16,27 @@ export const CurrencyChoice: React.FC<Props> = ({ onChange, value }) => { {
         {currency: "Japanese Yen", value: "JPY" },
         {currency: "Swiss Franc", value: "CHF" },
         {currency: "Australian Dollar", value: "AUD" },
+        {currency: "Argentine Peso", value: "ARS" },
+        {currency: "Chinese Yuan", value: "CNY" },
+        {currency: "Israeli New Shekel", value: "ILS" },
+        {currency: "New Zealand Dollar", value: "NZD" },
+        {currency: "New Turkish Lira", value: "TRY" },
+        {currency: "Mexican Peso", value: "MXN" },
+        {currency: "Polish Zloty", value: "PLN" },
+        {currency: "Swedish Krona", value: "SEK" },
+        {currency: "Singapore Dollar", value: "SGD" },
+        {currency: "Danish Crown", value: "DKK" },
+        {currency: "South African Rand", value: "ZAR" },
+        {currency: "Norwegian Krone", value: "NOK" },
+        {currency: "Hungarian Forint", value: "HUF" },
+        {currency: "czech crown", value: "CZK" },
+        {currency: "Thai Baht", value: "THB" },
+        {currency: "Emirates Dirham", value: "AED" },
 ] 
     
     return(
         <select 
-        className="bg-slate-900 focus:outline-none text-center"
+        className="bg-slate-900 focus:outline-none text-center" 
         value={value}
         onChange={onChange}
         >
